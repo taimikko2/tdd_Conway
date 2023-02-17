2d5e3756a4b0de708f07b99cf84f1d880d2b9e85
Mikko Saarinen <el4302@gmail.com>
2023-02-17 15:17:46 +0200

    test reading

--- test/Board.test.mjs
+++ test/Board.test.mjs
@@ -8,4 +8,15 @@ describe("Create Board", () => {
     bbb
     bbb`);
   });
+
+  it("Read RLE file", () => {
+    let b = new Board(1,1);
+    b.readRLE("G:/HY/conwayLife/blinker.rle")
+    // alussa kommenttisirivit pois #
+    // x = 3, y = 1, rule = B3/S23
+    // 3o!
+    // x = m, y = n
+    // width , height
+    expect(b.toString()).to.equalShape(`ooo`)
+  })
 });
