d196f5a156b1869b9e6ad2310fa77023a801ddf4
Mikko Saarinen <el4302@gmail.com>
2023-02-17 17:52:36 +0200

    simple file "3o!" -> "ooo"

--- src/Board.mjs
+++ src/Board.mjs
@@ -1,7 +1,13 @@
+import readTextFile from "read-text-file";
+//import lineReader  from 'line-reader';
+
 export class Board {
   width;
   height;
   canvas;
+  x;
+  y;
+
 
   constructor(w, h) {
     this.width = w;
@@ -13,10 +19,54 @@ export class Board {
     }
   }
 
+  async readRLE(filename) {
+    // "G:/HY/conwayLife/blinker.rle"
+    var content = readTextFile.readSync(filename);
+    console.log("luettiin "+content+" "+content.length);
+    //let cont = content.split("\n");
+    let h = 1;
+    let w = 3;
+    this.width = w;
+    this.height = h;
+    let res = "";
+    let count = 1;
+    this.canvas = new Array(h);
+    for (let i = 0; i < this.height; i++) {
+      this.canvas[i] = new Array(w);
+      //this.canvas[i].fill("b");
+    }
+    for (let i=0; i<content.length; i++) {
+      try {
+        if (content[i] in ["0","1","2","3","4","5","6","7","8","9"]) {
+          count = Number(content[i]); // erikoistapauksena kaksinumeroinen luku ...
+        } else {
+          throw new Error(content[i]+" ei ole luku");
+        }
+      } catch (err) {
+        if (content[i] == "!") {
+          break;
+        } 
+        for (let j = 0; j<count; j++) {
+          res += content[i];
+          this.canvas[0].push(content[i]);
+        }
+        count = 1;
+      }
+    }
+    //console.log("readRLE returning "+res)
+    return this.toString()
+    // alussa kommenttisirivit pois #
+    // x = 3, y = 1, rule = B3/S23
+    // 3o!
+    // x = m, y = n
+    // width , height
+    //(`ooo`)
+  }
+
   toString() {
     let res = "";
     for (let i = 0; i < this.height; i++) {
-      res += this.canvas[i].join("")+"\n";
+      res += this.canvas[i].join("") + "\n";
     }
     return res;
   }
--- test/Board.test.mjs
+++ test/Board.test.mjs
@@ -1,5 +1,6 @@
 import { expect } from "chai";
 import { Board } from "../src/Board.mjs";
+import writeFile from "write-file";
 
 describe("Create Board", () => {
   it("Create empty board", () => {
@@ -9,14 +10,24 @@ describe("Create Board", () => {
     bbb`);
   });
 
-  it("Read RLE file", () => {
-    let b = new Board(1,1);
-    b.readRLE("G:/HY/conwayLife/blinker.rle")
-    // alussa kommenttisirivit pois #
-    // x = 3, y = 1, rule = B3/S23
-    // 3o!
-    // x = m, y = n
-    // width , height
-    expect(b.toString()).to.equalShape(`ooo`)
-  })
+  it("Read simple RLE file '3o!' -> 'ooo' ", async () => {
+    const filename = "G:\\HY\\tdd\\tdd_Conway\\test_3o.rle";
+    let b = new Board(1, 1);
+    // kirjoita tiedosto, jossa on yksi rivi "3o!" ja lue se
+    await writeFile(filename, "3o!", function (err) {
+      if (err) return console.log("===? "+err)
+
+      console.log("Wrote file "+filename);
+    });
+    b = await b.readRLE(filename);
+    // 3o! -> 'ooo'
+    expect(b.toString()).to.equalShape(`ooo`);
+  });
+
+  // G:/HY/conwayLife/blinker.rle
+  // alussa kommenttisirivit pois #
+  // x = 3, y = 1, rule = B3/S23
+  // 3o!
+  // x = m, y = n
+  // width , height
 });
