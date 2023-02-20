8714a5c8c69d732398cf69f554b9e164793fda8f
Mikko Saarinen <el4302@gmail.com>
2023-02-20 16:54:51 +0200

    formatted

--- src/Board.mjs
+++ src/Board.mjs
@@ -39,7 +39,7 @@ export class Board {
       this.canvas[i] = new Array(w);
       //this.canvas[i].fill("b");
     }
-    this.contentToCanvas(0,0,content);
+    this.contentToCanvas(0, 0, content);
 
     return this.toString();
   }
@@ -47,15 +47,16 @@ export class Board {
   async addRLE(x, y, filename) {
     //console.log("addRLE(",x,",",y,",",filename,")");
     var content = readTextFile.readSync(filename);
-    this.contentToCanvas(x,y,content);
+    this.contentToCanvas(x, y, content);
     return this.toString();
   }
 
-  contentToCanvas(x,y,content) {
+  contentToCanvas(x, y, content) {
     let count = 0;
     let c = 0;
     let xpos = x;
-    console.log("content",content);
+    let ch;
+    console.log("content", content);
     for (let i = 0; i < content.length; i++) {
       try {
         if (content[i] in ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]) {
@@ -66,12 +67,16 @@ export class Board {
           throw new Error(content[i] + " ei ole luku");
         }
       } catch (err) {
+        //console.error("moka: ", err);
         if (content[i] == "!") {
           for (let j = xpos; j<this.width; j++) {
-            this.canvas[y][j] = "b";  
+            this.canvas[y][j] = "b";
           }
           break;
-        }
+        } else if (content[i] == "$") {
+          for (let j = xpos; j < this.width; j++) {
+            this.canvas[y][j] = "b";
+          }
         for (let j = 0; j < count; j++) {
           this.canvas[y][xpos] = content[i];
           console.log("write canvas ",y,xpos,content[i])
@@ -91,42 +96,42 @@ export class Board {
 
   tick() {
     let newCanvas = new Array(this.height);
-    for (let i=0; i< this.height; i++) {
+    for (let i = 0; i < this.height; i++) {
       newCanvas[i] = new Array(this.width);
     }
     for (let r = 0; r < this.height; r++) {
       for (let c = 0; c < this.width; c++) {
-        let count = 0; 
-        for (let ri = r-1; ri <= r+1; ri++) {
-          if (ri < 0 || ri>this.height-1) {
+        let count = 0;
+        for (let ri = r - 1; ri <= r + 1; ri++) {
+          if (ri < 0 || ri > this.height - 1) {
             continue;
           }
-          for (let ci = c-1; ci <= c+1; ci++) {
-            if (ci < 0 || ci>this.width-1) {
+          for (let ci = c - 1; ci <= c + 1; ci++) {
+            if (ci < 0 || ci > this.width - 1) {
               continue;
-            }  
+            }
             if (this.canvas[ri][ci] == "o") {
-              count ++;
+              count++;
             }
           }
         }
-        if (count in(this.survive) && this.canvas[r][c] == "o") {
+        if (count in this.survive && this.canvas[r][c] == "o") {
           newCanvas[r][c] = "o"; // survive
         } else if (count == this.birth) {
           newCanvas[r][c] = "o"; // birth
         } else {
-          newCanvas[r][c] = "b"; 
+          newCanvas[r][c] = "b";
         }
       }
     }
     this.canvas = newCanvas.slice();
-    return this
+    return this;
   }
 
   simulate(iterations) {
-    for (let i = 0; i<iterations; i++) {
+    for (let i = 0; i < iterations; i++) {
       this.tick();
-      console.log(this.toString())
+      console.log(this.toString());
     }
   }
 
